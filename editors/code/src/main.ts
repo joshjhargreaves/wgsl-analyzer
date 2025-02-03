c99c05ee6ef792ffd1831f2e550e368410b65649
Josh Hargreaves <jhargreaves@netflix.com>
2025-02-03 17:59:55 -0500

    0.9.3

--- editors/code/src/main.ts
+++ editors/code/src/main.ts
@@ -6,8 +6,8 @@ import * as fs from "fs";
 import * as util from "util";
 import * as cp from "child_process";
 
-const VERSION = "0.9.2";
-const REV = "fbb0d50";
+const VERSION = "0.9.3";
+const REV = "28629bf";
 
 const fileExists = (path: string) => util.promisify(fs.access)(path).then(s => true).catch(() => false);
 const exec = util.promisify(cp.exec);
